acf2121e7069c96ee1e413ba33a1f77311906c4d
Efe Yolartıran <70057764+efeyol1@users.noreply.github.com>
2024-02-27 14:07:08 +0300

    Create myFirstLabScript.py
    python version of lab

---
+++ myFirstLabScript.py
@@ -0,0 +1,31 @@
+a=input("what is your name:")
+print("hello " , a)
+b=input("what is your student id:")
+print("your student id is " , b)
+
+
+var1= int(input("enter first value"))
+var2 = int(input("enter second value"))
+sum = var1+var2
+diff = var1-var2
+prod = var1*var2
+print("sum:" , sum)
+print("diff: " , diff)
+print("prod: " , prod)
+print("var1:", var1)
+print("var2:" , var2)
+
+name=input("what is your name:")
+lab = int(input("whatis your lab grade:"))
+midterm = int(input("what is your midterm grade:"))
+final =int(input("what is your final grade:"))
+last_score = (lab/4)+(midterm*35/100)+(final*40/100)
+
+print("name: ", name)
+print("lab: ", lab)
+print("midterm: ", midterm)
+print("final: ", final)
+print("last score: ", last_score)
+
+
+print("*\n**\n***\n**\n*")
